3f7a4869c1e02d6359d209faffaceca9fcaea8c4
Lazaro1 <lazarolimasilva2@gmail.com>
2021-07-12 16:51:57 -0300

    initial app and tests with styled components

---
+++ src/Home/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import {Container, Tittle} from './styles'
+
+export default function Home () {
+    return (
+        <Container >
+            <Tittle > Essa é a Home</Tittle>
+        </Container>
+    )
+}
+
---
+++ src/Home/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    flex:1;
+    justify-content: center;
+    align-items: center;
+    background-color: #121212
+`;
+
+export const Tittle = styled.Text`
+    color: white
+`;
